15fbc2fddaaeb756db6d7cf42e2e32fe227b18bd
bibidu <787753295@qq.com>
2020-05-31 11:47:11 +0800

    feat: 添加简单的权限校验、修改样式

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, './static')))
 let routes = [] // 项目路由配置
 
 const baseDir = './docs'
+let startTime = new Date().getTime()
 
 const getSidebar = () => getMdAndResolve(`${baseDir}/_sidebar.md`)
 
@@ -27,14 +28,20 @@ setup()
 
 function startServer() {
   app.get('*', async (req, res) => {
-
     if (req.url === '/favicon.ico') {
       res.setHeader("Content-Type", 'image/x-icon')
       return res.end(fs.readFileSync('./favicon.ico'))
     }
+    const hasAuth = req.url.includes('from=5213')
+    console.log(req.url)
+    if (hasAuth) {
+      req.url = req.url.replace(/\?from=5213$/, '')
+    } else {
+      if (req.url === '/') {
+        renderNull(res)
+      }
+    }
     const matched = getMatchedRoute(req.url)
-    
-  
     if (matched) {
       const { url, name } = matched
       const mayExistFilepath = `${baseDir}${url}.md`
@@ -42,6 +49,7 @@ function startServer() {
         const content = await getMdAndResolve(mayExistFilepath)
         return res.end(
           renderHtml({
+            showSidebar: Boolean(hasAuth),
             title: name,
             content: content + `
             <script>
@@ -53,14 +61,8 @@ function startServer() {
         )
       }
     }
-  
-    res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' })
-    res.end(
-      renderHtml({
-        title: 404,
-        content: '404'
-      })
-    )
+    
+    renderNull(res)
   })
   
   app.listen(8080, () => {
@@ -68,6 +70,15 @@ function startServer() {
   })
 }
 
+function renderNull(res) {
+  res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' })
+  res.end(
+    renderHtml({
+      title: 404,
+      content: '404',
+    })
+  )
+}
 function getMatchedRoute(url) {
   if (url === '/') return routes[0]
   for (let i = 0; i < routes.length; i++) {
@@ -78,17 +89,32 @@ function getMatchedRoute(url) {
   }
 }
 
-function renderHtml({ title, content, cssType }) {
+function renderHtml({
+  showSidebar = false,
+  title,
+  content,
+  cssType,
+}) {
   const css = cssType ? '' : fs.readFileSync('./main.css', 'utf8')
   const baseHtml = fs.readFileSync('./base.html', 'utf8')
   const sidebarHtml = routes.reduce(
     (p, {name, url}) => `${p}<a href="${url}" class="sidebar-item">${name}</a>`,
     ''
   )
-
   return baseHtml
     .replace(/\{title}/g, title)
     .replace(/\{style}/g, css)
     .replace(/\{content}/g, content)
-    .replace(/\{sidebar}/g, sidebarHtml)
+    .replace(/\{sidebar}/g, showSidebar ? sidebarHtml : `
+    <script type="">
+      document.querySelector('.sidebar').style.display = 'none'
+      const styles = {
+        width: '800px',
+        minWidth: '70%',
+        margin: '20px auto',
+        boxShadow: '0 2px 12px rgba(0,0,0,0.2)'
+      }
+      Object.entries(styles).forEach(([k, v]) => document.querySelector('.app').style[k] = v)
+    </script>
+    `)
 }
